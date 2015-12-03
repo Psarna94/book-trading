bfaf14d47fa8e5bac1e5d8935da7945eb8eb068c
Pranu Sarna <pranusarna@gmail.com>
2015-12-03 20:29:09 +0530

    Intialized the user endpoint

--- server/auth/index.js
+++ server/auth/index.js
@@ -3,3 +3,11 @@
 var express = require('express');
 var passport = require('passport');
 var config = require('..config/environment');
+var User = require('../api/user/user.model.js');
+
+require('./local/passport').setup(User,config);
+
+var router = express.Router();
+router.use('/local', require('./local'));
+
+module.exports = router;
