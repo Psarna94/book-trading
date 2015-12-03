d9c61bddfd9ef0f072581cddd3845c290a568b28
Pranu Sarna <pranusarna@gmail.com>
2015-12-03 21:00:20 +0530

    Some changes in user model still remain

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -1,12 +1,19 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var crypto = require('crypto');
 
 var UserSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+    name          : String,
+    email         : {type: String, lowercase: true},
+    role          : {
+        type   : String,
+        default: 'user'
+    },
+    hashedPassword: String,
+    provider      : String,
+    salt          : String
 });
 
 module.exports = mongoose.model('User', UserSchema);
